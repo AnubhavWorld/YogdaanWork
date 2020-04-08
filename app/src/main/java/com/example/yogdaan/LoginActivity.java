0124d0eaac21bb2dfd422c3a0e10cfaf177448fb
Anubhav <18ucs066@lnmiit.ac.in>
2020-04-08 16:23:13 +0530

    firebase-firestore

--- app/src/main/java/com/example/yogdaan/LoginActivity.java
+++ app/src/main/java/com/example/yogdaan/LoginActivity.java
@@ -1,14 +1,59 @@
 package com.example.yogdaan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button login_button;
+    private Button create_account_button;
+    private EditText username_text;
+    private EditText password_text;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login_button=findViewById(R.id.login_login);
+        auth=FirebaseAuth.getInstance();
+        create_account_button=findViewById(R.id.login_create);
+        create_account_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,SignupActivity.class));
+            }
+        });
+        username_text=findViewById(R.id.login_username);
+        password_text=findViewById(R.id.login_password);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signInWithEmailAndPassword(username_text.getText().toString()+"@gmail.com",password_text.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this,"unsuccessful",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
     }
+
 }
+
--- app/src/main/java/com/example/yogdaan/SignupActivity.java
+++ app/src/main/java/com/example/yogdaan/SignupActivity.java
@@ -1,14 +1,31 @@
 package com.example.yogdaan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
 
+    private Button login_button;
+    private Button create_account_button;
+    private EditText username_text;
+    private EditText password_text;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
     }
+
 }
