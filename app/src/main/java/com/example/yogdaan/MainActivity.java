12473f36403aa49e623dca1d8baaadd907570059
Viren2737 <virensaroha123@gmail.com>
2020-04-08 13:26:30 +0530

    viren

--- app/src/main/java/com/example/yogdaan/MainActivity.java
+++ app/src/main/java/com/example/yogdaan/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.yogdaan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button Btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Btn = (Button)findViewById(R.id.btn);
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hii there....", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
