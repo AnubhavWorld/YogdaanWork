739b8f6d5604069c02107a57d89e5d060bd7893c
Vishnu Bhat <vishnubhat820@gmail.com>
2020-04-08 19:01:26 +0530

    Homepage

--- app/src/main/java/com/example/yogdaan/MainActivity.java
+++ app/src/main/java/com/example/yogdaan/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.yogdaan;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,19 +13,88 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button Btn;
+
+    DrawerLayout d;
+    Toolbar t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Btn = (Button)findViewById(R.id.btn);
-        Btn.setOnClickListener(new View.OnClickListener() {
+        setuptoolbarnow();
+        CardView v1= findViewById(R.id.i1);
+        v1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,donationActivity.class);
                 startActivity(intent);
+
             }
         });
+
+
+        CardView v2= findViewById(R.id.i2);
+        v2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Have a cup of coffee,bud!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        CardView v3= findViewById(R.id.i3);
+        v3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Welcome to my favorite place!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        CardView v4= findViewById(R.id.a1);
+        v4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hold my beer", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        CardView v5= findViewById(R.id.a2);
+        v5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "LUBBBBB", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        CardView v6= findViewById(R.id.a3);
+        v6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "HELLO", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+
+
+
+
+
     }
+
+    private void setuptoolbarnow() {
+        d=  findViewById(R.id.d1);
+        t=  findViewById(R.id.t1);
+
+        ActionBarDrawerToggle ac= new ActionBarDrawerToggle(this,d,t,R.string.app_name,R.string.app_name);
+        d.addDrawerListener(ac);
+        ac.syncState();
+
+
+
+
+    }
+
+
 }
