3526a99dce6129bf48a4b940fcb8d90b7eba0381
Viren2737 <virensaroha123@gmail.com>
2020-04-09 11:58:33 +0530

    donate food

--- app/src/main/java/com/example/yogdaan/donationActivity.java
+++ app/src/main/java/com/example/yogdaan/donationActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class donationActivity extends AppCompatActivity {
-    private Button donateMoneyBtn;
+    private Button donateMoneyBtn,donateClothesBtn,donateFoodBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +16,27 @@ public class donationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_donation);
 
         donateMoneyBtn =(Button)findViewById(R.id.donate_money_btn);
+        donateClothesBtn =(Button)findViewById(R.id.donate_clothes_btn);
+        donateFoodBtn =(Button)findViewById(R.id.donate_food_btn);
 
         donateMoneyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(donationActivity.this,paymentActivity.class);
+                startActivity(intent);
+            }
+        });
+        donateClothesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(donationActivity.this,donateClothesActivity.class);
+                startActivity(intent);
+            }
+        });
+        donateFoodBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(donationActivity.this,donateFoodActivity.class);
                 startActivity(intent);
             }
         });
